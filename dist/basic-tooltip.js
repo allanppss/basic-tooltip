4bb49b0dbd1911491bbeb7e2c243d3153848de80
Allan Soares <apsouza@marketdata.com.br>
2020-04-08 18:30:03 -0300

    attr color ok

--- dist/basic-tooltip.js
+++ dist/basic-tooltip.js
@@ -12,6 +12,7 @@ var AppBasicTooltip = function () {
             let classes = '';
             let bg = '';
             let fill = '';
+            let color = '';
             let msg = $(element).attr('data-bt-msg');
 
             // Se contem alinhamento
@@ -25,8 +26,13 @@ var AppBasicTooltip = function () {
                 fill = `fill: ${$(element).attr('data-bt-bg')};`;
             };
 
+            //Se contem Color
+            if ($(element).attr('data-bt-color')) {
+                color = `color: ${$(element).attr('data-bt-color')};`;
+            };
+
             $(element).append(`
-                <span class="basic-tooltip ${classes}" style="${bg}">
+                <span class="basic-tooltip ${classes}" style="${bg}${color}">
                     ${msg}
                     <svg style="${fill}" height="30" width="30" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg"><path d="M14 20l10 10 10-10z"/><path d="M0 0h48v48h-48z" fill="none"/></svg>
                 </span>
