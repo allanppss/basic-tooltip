40491289979298c99c966d641bdfdc3972a0a6a8
Allan Soares <apsouza@marketdata.com.br>
2020-04-08 18:10:16 -0300

    attr bg ok

--- dist/basic-tooltip.js
+++ dist/basic-tooltip.js
@@ -10,14 +10,26 @@ var AppBasicTooltip = function () {
     let putTooltipsElements = function (elements, qtdEl) {
         elements.map((index, element) => {
             let classes = '';
+            let bg = '';
+            let fill = '';
             let msg = $(element).attr('data-bt-msg');
-            
+
+            // Se contem alinhamento
             if ($(element).attr('data-bt-align')) {
                 classes += 'align-' + $(element).attr('data-bt-align');
             };
 
+            //Se contem BG
+            if ($(element).attr('data-bt-bg')) {
+                bg = `background: ${$(element).attr('data-bt-bg')};`;
+                fill = `fill: ${$(element).attr('data-bt-bg')};`;
+            };
+
             $(element).append(`
-                <span class="basic-tooltip ${classes}">${msg}</span>
+                <span class="basic-tooltip ${classes}" style="${bg}">
+                    ${msg}
+                    <svg style="${fill}" height="30" width="30" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg"><path d="M14 20l10 10 10-10z"/><path d="M0 0h48v48h-48z" fill="none"/></svg>
+                </span>
             `);
         });
     }
